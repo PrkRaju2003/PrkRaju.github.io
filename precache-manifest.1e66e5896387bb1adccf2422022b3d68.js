d80fac0e7547a3addda7167b0a4d09d6c79e7026
PrkRaju2003 <pusapati.raju.rk@gmail.com>
2022-08-05 10:32:41 +0530

    Updates

---
+++ precache-manifest.1e66e5896387bb1adccf2422022b3d68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1616d7fa7955353a06",
+    "url": "/resume/static/css/main.3a05ebf3.chunk.css"
+  },
+  {
+    "revision": "fe1616d7fa7955353a06",
+    "url": "/resume/static/js/main.1eaf1953.chunk.js"
+  },
+  {
+    "revision": "0151e26418218b470cc6",
+    "url": "/resume/static/js/runtime~main.6e209624.js"
+  },
+  {
+    "revision": "91598fd8c7b9d1930291",
+    "url": "/resume/static/js/2.e715c1b1.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/resume/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "931cde8040479462af811c8b9a44bb48",
+    "url": "/resume/static/media/profilepic.931cde80.jpg"
+  },
+  {
+    "revision": "be13da44040de1455f17fc2963bbb8ad",
+    "url": "/resume/static/media/Breathe.be13da44.jpg"
+  },
+  {
+    "revision": "5db4ae71a394c35a92459e106f054973",
+    "url": "/resume/static/media/kkr.5db4ae71.png"
+  },
+  {
+    "revision": "d2262ff2df7f7e684337775837660176",
+    "url": "/resume/static/media/timp.d2262ff2.png"
+  },
+  {
+    "revision": "101bf09455d615522aa158d9178018aa",
+    "url": "/resume/index.html"
+  }
+];
